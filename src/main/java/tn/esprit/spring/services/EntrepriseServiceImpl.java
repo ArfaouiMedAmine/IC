fe857e716a58bdc1b00d9ff4fbe33f720f1ba58b
ghassen68 <ghassen.nouri@esprit.tn>
2020-11-05 19:55:09 +0100

    test

--- src/main/java/tn/esprit/spring/services/EntrepriseServiceImpl.java
+++ src/main/java/tn/esprit/spring/services/EntrepriseServiceImpl.java
@@ -3,6 +3,8 @@ package tn.esprit.spring.services;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,8 +22,13 @@ public class EntrepriseServiceImpl implements IEntrepriseService {
 	@Autowired
 	DepartementRepository deptRepoistory;
 	
+	private static final Logger l = LogManager.getLogger(EntrepriseServiceImpl.class);
+	
+	
 	public int ajouterEntreprise(Entreprise entreprise) {
+		l.info("In  ajouterEntreprise : " + entreprise);
 		entrepriseRepoistory.save(entreprise);
+		l.info("Out of  ajouterEntreprise. ");
 		return entreprise.getId();
 	}
 
--- src/test/java/tn/esprit/spring/EntrepriseServiceImplTest.java
+++ src/test/java/tn/esprit/spring/EntrepriseServiceImplTest.java
@@ -1,25 +1,35 @@
 package tn.esprit.spring;
 
 import static org.junit.Assert.assertEquals;
-
 import java.text.ParseException;
+import java.util.List;
+
+import javax.transaction.Transactional;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import tn.esprit.spring.entities.Employe;
 import tn.esprit.spring.entities.Entreprise;
+import tn.esprit.spring.repository.EntrepriseRepository;
 import tn.esprit.spring.services.IEntrepriseService;
 
 @RunWith(SpringRunner.class)
+@Transactional // tester sans faire des modifications au niveau de la base de données
 @SpringBootTest
 public class EntrepriseServiceImplTest {
 	@Autowired
 	IEntrepriseService entropoService ;
 	
+	@Autowired
+    EntrepriseRepository entrepriseRepoistory;
+	
 	@Test
+	@Rollback(false)
 	public void testAddOrUpdateEmploye() throws ParseException {
 	Entreprise entreprise = new Entreprise();
 	entreprise.setName("esprit");
@@ -27,4 +37,53 @@ public class EntrepriseServiceImplTest {
 	int entropoId = entropoService.ajouterEntreprise(entreprise);
 	assertEquals(entropoId, entreprise.getId());
 	}
+	
+	
+	/* @Test
+	public void testDeleteProduct()throws ParseException {
+		Entreprise entreprise = entropoService.getEntrepriseById(28);
+	     
+		entropoService.deleteEntrepriseById(entreprise.getId());
+	     
+		Entreprise deletedEntropo = null ;
+		
+		try { 
+			
+			deletedEntropo = entrepriseRepoistory.findById(28).get() ;
+			assertEquals(1, 0);
+			
+		}
+		catch (NoSuchElementException e ) {
+			assertEquals(1, 1);
+
+			
+		}
+		*/
+
+	@Test
+	@Rollback(true)
+	public void testDeleteProduct() throws ParseException {
+		Entreprise entreprise = entropoService.getEntrepriseById(33);
+
+		entropoService.deleteEntrepriseById(entreprise.getId());
+		try {
+			Entreprise deletedEntreprise = entropoService.getEntrepriseById(33);
+
+		} catch (Exception e) {
+
+			Entreprise deletedEntreprise = null;
+			assertEquals(deletedEntreprise, null);
+		}
+
+	}
+	     
+	 
+
+
+	 
+	     
+	
+	
+	
+	
 }
